40f51cde16e508d437126ed90907dab4ab98e886
JC Bombardelli <jc.bombardelli@live.com>
2021-04-02 19:04:22 -0300

    Dia 03 - Solução do Desafio (sempre dá pra melhorar)

--- dia-03/03-requests/index.js
+++ dia-03/03-requests/index.js
@@ -1,13 +1,36 @@
-console.log(process.env.NODE_ENV)
 const fetch = require('node-fetch')
 
-const getPerson = (id) => {
+
+
+function getPerson(id){
     fetch(`https://swapi.dev/api/people/${id}`)
         .then(response => response.json())
-        .then(person => console.log(person))
-        .catch(err => console.error(err))
-        .finally(() => 'termino da request')
+        .then(async (person) => {
+
+            const { homeworld, films, vehicles, starships} = person
+
+            const promiseHomeWorld =  await fetch(homeworld).then(response => response.json())
+            person.homeworld = promiseHomeWorld.name
+
+            const promiseFilms = films.map(url => fetch(url).then((response) => response.json()))
+            const promiseVehicles = vehicles.map(url => fetch(url).then(response => response.json()))
+            const promisesStartships = starships.map(url => fetch(url).then(response => response.json()))
+
+            const startTime = Date.now()
+            Promise.all([promiseFilms, promiseVehicles, promisesStartships])
+                .then(async (results) => {
+                    await Promise.all(results[0]).then(films => films.forEach((film, index) => person.films[index] = film.title))
+                    await Promise.all(results[1]).then(vehicles => vehicles.forEach((vehicle, index) => person.vehicles[index] = vehicle.name))
+                    await Promise.all(results[2]).then(starships => starships.forEach((starship, index) => person.starships[index] = starship.name))
+                })
+                .finally(() => {
+                    console.log(person)
+                    console.info(`Finished in ${(Date.now() - startTime) / 1000} seconds`)
+                })
+        
+            
+        })
+              
+
 }
-
-
 getPerson(1)
